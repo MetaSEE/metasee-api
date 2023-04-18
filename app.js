94a25e015c7d9e321d88e15b69476a36c555cd85
Filipe Arantes Fernandes <filran@gmail.com>
2023-04-18 10:16:02 -0300

    cors updated

--- app.js
+++ app.js
@@ -19,6 +19,20 @@ var virtualworldRouter = require('./routes/VirtualWorld');
 
 var app = express();
 
+/**
+ * CORS configuration.
+*/
+var cors = require("cors");
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
