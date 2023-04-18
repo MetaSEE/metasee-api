6c1d58949470d8daa7eecd30c1b31e74b4fd33c0
Filipe Arantes Fernandes <filran@gmail.com>
2023-04-18 11:26:37 -0300

    cors updated

--- app.js
+++ app.js
@@ -23,14 +23,20 @@ var app = express();
  * CORS configuration.
 */
 var cors = require("cors");
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    app.use(cors());
-    next();
-});
+// app.use((req, res, next) => {
+// 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+//     res.header("Access-Control-Allow-Origin", "*");
+// 	//Quais são os métodos que a conexão pode realizar na API
+//     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//     app.use(cors());
+//     next();
+// });
+var corsOptions = {
+  // origin: 'http://metasee-virtualworld.herokuapp.com/',
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 
 // view engine setup
